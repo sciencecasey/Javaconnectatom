6efdfd49d7e74448a41fa0ad4c1cc9d4c7258a5d
Casey Jayne <sciencecaseyj@gmail.com>
2020-02-27 18:16:39 -0800

    Add files via upload

--- MorseTranslator.java
+++ MorseTranslator.java
@@ -6,21 +6,21 @@ public class MorseTranslator {
 
    public static void main(String[] args) {
       //Option to input arguments
-      //to use this option, arg[0] is the starting language, arg[1] the input to 
+      //to use this option, arg[0] is the starting language, arg[1] the input to
       //translate.
       //ex: java MorseTranslator M '-... .'
-      
+
       //Create Variables
       String startLang= new String();
       String input = new String();
       //Setup Scanner
       Scanner scan = new Scanner(System.in);
-      
+
       //grab input
       if (args.length>0) {
          startLang = args[0].trim().toUpperCase();
          input = args[1].trim().toUpperCase();
-      } 
+      }
       else {//user didn't input args, prompt
          System.out.println("What language would you like to translate from?");
          System.out.println("enter M for Morse Code and E for English");
@@ -32,7 +32,7 @@ public class MorseTranslator {
          input = scan.nextLine().trim().toUpperCase();
       }//end getting input
       scan.close();
-      
+
       //Call Morse Cascading methods
       //Call Instance
       MorseTranslationMethods translate = new MorseTranslationMethods();
